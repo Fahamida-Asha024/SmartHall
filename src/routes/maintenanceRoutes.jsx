8131beadb6ff4874773f2059a1fb5cec76bb45e9
raisuuuu2023 <raisa22025000@gmail.com>
2026-08-31 13:32:54 +0600

    added maintenance profile page

--- src/routes/maintenanceRoutes.jsx
+++ src/routes/maintenanceRoutes.jsx
@@ -2,38 +2,39 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import MaintenanceLayout from "../layouts/MaintenanceLayout";
-
 import MaintenanceDashboardPage from "../pages/maintenance/MaintenanceDashboardPage";
 import TaskDetailsPage from "../pages/maintenance/TaskDetailsPage";
 import WorkHistoryPage from "../pages/maintenance/WorkHistoryPage";
 import MaintenanceNotificationsPage from "../pages/maintenance/MaintenanceNotificationsPage";
+import MaintenanceProfilePage from "../pages/maintenance/MaintenanceProfilePage";
 
 export default function MaintenanceRoutes() {
   return (
     <Route path="/maintenance" element={<MaintenanceLayout />}>
 
-      {/* Dashboard */}
       <Route
         path="dashboard"
         element={<MaintenanceDashboardPage />}
       />
 
-      {/* Individual Task */}
       <Route
         path="tasks/:id"
         element={<TaskDetailsPage />}
       />
 
-      {/* Work History */}
       <Route
         path="history"
         element={<WorkHistoryPage />}
       />
 
-      {/* Notifications */}
       <Route
         path="notifications"
         element={<MaintenanceNotificationsPage />}
+      />
+
+      <Route
+        path="profile"
+        element={<MaintenanceProfilePage />}
       />
 
     </Route>
